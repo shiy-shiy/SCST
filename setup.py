018a23e8e6656f8e174351ee423482177ad6e669
shiy-shiy <74085221+shiy-shiy@users.noreply.github.com>
2024-03-18 22:16:06 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Single-cell RNA sequencing data excels in providing high sequencing depth and precision at the single-cell level, but lacks spatial information. Simultaneously, spatial transcriptomics technology visualizes gene expression patterns in their spatial context but has low resolution. Here, we present COSCST that combines these two datasets through autoencoder and supervised learning model to map single-cell RNA-seq data with spatial coordination and spatial transcriptomics with precise cell type annotation. ',   # Give a short description about your library
-  author = 'Yi Shi ,Gang Hu',                   # Type in your name
+  author = 'Yi Shi, Gang Hu',                   # Type in your name
   author_email = 'shiyi@nankai.mail.edu.cn, huggs@nankai.edu.cn',      # Type in your E-Mail
   url = 'https://github.com/shiy-shiy/SCST/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Yiguan/Cookie2Dict/archive/master.zip',
+  download_url = 'https://github.com/shiy-shiy/SCST/archive/refs/heads/main.zip',
   keywords = ['single cell', 'spatial transcriptome'],   # Keywords that define your package best
   install_requires=['matplotlib>=2.2'
 				#'pydot', 
